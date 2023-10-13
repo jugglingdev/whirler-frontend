1c26aba2ffdc1ad19cb59541c08e0d9f3b75ba9b
jugglingdev <kaylamarie1785@gmail.com>
2023-10-12 22:13:21 -0500

    Update Dashboard carousel list layout

--- src/app/shared/carousel/carousel.component.ts
+++ src/app/shared/carousel/carousel.component.ts
@@ -15,7 +15,7 @@ export class CarouselComponent {
 
   constructor() {
     this.carousel = new Carousel(
-      'Breakout Post',
+      'Test Post',
       'In this post, I tell my story - what got me into coding',
       ['softwaredevelopment', 'careerswitcher'],
       'https://media.istockphoto.com/id/1224500457/photo/programming-code-abstract-technology-background-of-software-developer-and-computer-script.jpg?s=612x612&w=0&k=20&c=nHMypkMTU1HUUW85Zt0Ff7MDbq17n0eVeXaoM9Knt4Q=',
