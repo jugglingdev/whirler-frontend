a62954263d943d42473084d7bd15034ad45d0889
jugglingdev <kaylamarie1785@gmail.com>
2023-12-07 20:30:00 -0600

    Update Carousel model

--- src/app/shared/carousel.model.ts
+++ src/app/shared/carousel.model.ts
@@ -1,12 +1,10 @@
-import { Slide } from "../carousel-edit/slide/slide.model";
-
 export class Carousel {
 
   constructor (
     public title: string,
     public description: string,
+    public tags: string[],
     public thumbnail: string,
-    public slides: Slide[]
   ){};
 
 }
