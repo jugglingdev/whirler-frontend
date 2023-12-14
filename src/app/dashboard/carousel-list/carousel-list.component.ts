8e0920ea860975e069a4cdbe46a946efbde02f48
jugglingdev <kaylamarie1785@gmail.com>
2023-12-13 23:27:13 -0600

    Remove hard coded CarouselListService; replace with CarouselService synced with database

--- src/app/dashboard/carousel-list/carousel-list.component.ts
+++ src/app/dashboard/carousel-list/carousel-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { CarouselListService } from 'src/app/dashboard/carousel-list/carousel-list.service';
 import { Carousel } from 'src/app/shared/carousel.model';
+import { CarouselService } from 'src/app/shared/carousel.service';
 
 @Component({
   selector: 'app-carousel-list',
@@ -11,14 +11,22 @@ export class CarouselListComponent implements OnInit {
 
   carousels: Carousel[];
 
-  constructor (private carouselListService: CarouselListService) {}
+  constructor (private carouselService: CarouselService) {}
 
   ngOnInit(): void {
-    this.carousels = this.carouselListService.getCarousels();
-    this.carouselListService.carouselsUpdated.subscribe((carousels) => this.carousels = carousels);
+    this.carouselService.getAllCarousels()
+      .subscribe((data) => {
+        if (data && typeof data === 'object') {
+          this.carousels = Object.values(data);
+        } else {
+          this.carousels = data;
+        }
+      });
   }
 
   removeCarousel(id: number) {
-    this.carouselListService.removeCarousel(id);
+    this.carouselService.deleteCarousel(id).subscribe(() => {
+
+    });
   }
 }
--- src/app/shared/carousel.service.ts
+++ src/app/shared/carousel.service.ts
@@ -7,28 +7,29 @@ import { Observable } from "rxjs";
 export class CarouselService {
 
   private carousel: Carousel;
+  private carousels: Carousel[];
   private baseUrl = 'https://whirler-6dbae-default-rtdb.firebaseio.com/';
 
-  private carousels: Carousel[] = [
-    new Carousel(
-      'Breakout Post',
-      'In this post, I tell my story - what got me into coding',
-      ['softwaredevelopment', 'careerswitcher'],
-      'https://media.istockphoto.com/id/1224500457/photo/programming-code-abstract-technology-background-of-software-developer-and-computer-script.jpg?s=612x612&w=0&k=20&c=nHMypkMTU1HUUW85Zt0Ff7MDbq17n0eVeXaoM9Knt4Q='
-    ),
-    new Carousel(
-      'Dev Puns',
-      'Jokes about software development',
-      ['softwaredevelopment', 'jokes'],
-      'https://images.unsplash.com/photo-1665686377065-08ba896d16fd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bGF1Z2hpbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
-    ),
-    new Carousel(
-      'What a Codefied World',
-      'A parody of "What a Wonderful World" for software developers',
-      ['softwaredevelopment', 'music'],
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjM-Ei5zpMh9ikJZPZrArGWLSit_aSYDKxKxYEZpE62U8YklAebLX53-6fW0wBmqO6pZYI&s'
-    ),
-  ]
+  // private carousels: Carousel[] = [
+  //   new Carousel(
+  //     'Breakout Post',
+  //     'In this post, I tell my story - what got me into coding',
+  //     ['softwaredevelopment', 'careerswitcher'],
+  //     'https://media.istockphoto.com/id/1224500457/photo/programming-code-abstract-technology-background-of-software-developer-and-computer-script.jpg?s=612x612&w=0&k=20&c=nHMypkMTU1HUUW85Zt0Ff7MDbq17n0eVeXaoM9Knt4Q='
+  //   ),
+  //   new Carousel(
+  //     'Dev Puns',
+  //     'Jokes about software development',
+  //     ['softwaredevelopment', 'jokes'],
+  //     'https://images.unsplash.com/photo-1665686377065-08ba896d16fd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bGF1Z2hpbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
+  //   ),
+  //   new Carousel(
+  //     'What a Codefied World',
+  //     'A parody of "What a Wonderful World" for software developers',
+  //     ['softwaredevelopment', 'music'],
+  //     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjM-Ei5zpMh9ikJZPZrArGWLSit_aSYDKxKxYEZpE62U8YklAebLX53-6fW0wBmqO6pZYI&s'
+  //   ),
+  // ]
 
   constructor(private http: HttpClient) {}
 
@@ -53,10 +54,10 @@ export class CarouselService {
         newCarousel);
   }
 
-  updateCarousel(updatedCarousel: Carousel): Observable<any> {
+  updateCarousel(carouselId: string, updatedCarousel: Carousel): Observable<any> {
     return this.http
       .put(
-        `${this.baseUrl}/carousels.json`,
+        `${this.baseUrl}/carousels/${carouselId}.json`,
         updatedCarousel);
   }
 
