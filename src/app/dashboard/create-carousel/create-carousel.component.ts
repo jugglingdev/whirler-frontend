bc7eba1efe0e918a2838633e585c4eef9ed2d69f
jugglingdev <kaylamarie1785@gmail.com>
2023-12-17 23:08:29 -0600

    Fix body scroll bug when modal is open

--- src/app/dashboard/create-carousel/create-carousel.component.ts
+++ src/app/dashboard/create-carousel/create-carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { CarouselService } from 'src/app/shared/carousel.service';
 import { Router } from '@angular/router';
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
   templateUrl: './create-carousel.component.html',
   styleUrl: './create-carousel.component.scss'
 })
-export class CreateCarouselComponent implements OnInit {
+export class CreateCarouselComponent implements OnInit, OnDestroy {
 
   carouselForm: FormGroup;
   @Output('closeModal') closeModal = new EventEmitter<void>();
@@ -17,12 +17,14 @@ export class CreateCarouselComponent implements OnInit {
   constructor(private carouselService: CarouselService, private router: Router) {}
 
   ngOnInit(): void {
-      this.carouselForm = new FormGroup({
-        title: new FormControl(null, Validators.required),
-        description: new FormControl(null),
-        tags: new FormArray([]),
-        thumbnail: new FormControl(null)
-      });
+    document.body.classList.add('no-scroll');
+
+    this.carouselForm = new FormGroup({
+      title: new FormControl(null, Validators.required),
+      description: new FormControl(null),
+      tags: new FormArray([]),
+      thumbnail: new FormControl(null)
+    });
   }
 
   get tags() {
@@ -64,6 +66,10 @@ export class CreateCarouselComponent implements OnInit {
 
   onCloseModal() {
     this.closeModal.emit();
+  }
+
+  ngOnDestroy(): void {
+    document.body.classList.remove('no-scroll');
   }
 
 }
