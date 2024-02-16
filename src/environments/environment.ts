60e950f75ccd6a55f72f95a4efe424979499e84d
jugglingdev <kaylamarie1785@gmail.com>
2024-02-15 19:05:14 -0600

    Update prod env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   firebaseUrl: 'https://whirler-6dbae-default-rtdb.firebaseio.com/',
-  production: false,
+  production: true,
   signupUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAGHDpvUo1PpymtFENUw9iOaosowOApa9Q',
   loginUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAGHDpvUo1PpymtFENUw9iOaosowOApa9Q'
 };
