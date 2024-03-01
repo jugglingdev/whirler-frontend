dc8a0409d270c9112e791fc208a251c00872cb7d
jugglingdev <kaylamarie1785@gmail.com>
2024-02-29 19:28:48 -0600

    Add http methods to slide service

--- src/app/services/slide.service.ts
+++ src/app/services/slide.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Slide } from '../models/slide';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SlideService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getSlides(): Observable<Slide[]> {
+    return this.http.get<Slide[]>(`${environment.apiUrl}/slides`);
+  }
+
+  getSlideById(id: number): Observable<Slide> {
+    return this.http.get<Slide>(`${environment.apiUrl}/slides/${id}`);
+  }
+
+  createSlide(slide: Slide): Observable<Slide> {
+    return this.http.post<Slide>(`${environment.apiUrl}/slides`, slide);
+  }
+
+  updateSlide(slide: Slide): Observable<Slide> {
+    return this.http.put<Slide>(`${environment.apiUrl}/slides/${slide.id}`, slide);
+  }
+
+  deleteSlide(id: number): Observable<Slide> {
+    return this.http.delete<Slide>(`${environment.apiUrl}/slides/${id}`);
+  }
 }
