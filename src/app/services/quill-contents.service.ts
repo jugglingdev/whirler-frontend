3ae5358a56c2adf75ea07f359e820f2687728e21
jugglingdev <kaylamarie1785@gmail.com>
2024-02-29 19:35:33 -0600

    Add http methods to quill-contents service

--- src/app/services/quill-contents.service.ts
+++ src/app/services/quill-contents.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { QuillContent } from '../models/quill-content';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuillContentsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getQuillContents(): Observable<QuillContent[]> {
+    return this.http.get<QuillContent[]>(`${environment.apiUrl}/quill-contents`);
+  }
+
+  getQuillContentsById(id: number): Observable<QuillContent> {
+    return this.http.get<QuillContent>(`${environment.apiUrl}/quill-contents/${id}`);
+  }
+
+  createQuillContent(quillContent: QuillContent): Observable<QuillContent> {
+    return this.http.post<QuillContent>(`${environment.apiUrl}/quill-contents`, quillContent);
+  }
+
+  updateQuillContent(quillContent: QuillContent): Observable<QuillContent> {
+    return this.http.put<QuillContent>(`${environment.apiUrl}/quill-contents/${quillContent.id}`, quillContent);
+  }
+
+  deleteQuillContent(id: number): Observable<QuillContent> {
+    return this.http.delete<QuillContent>(`${environment.apiUrl}/quill-contents/${id}`);
+  }
 }
