e373d51660a16a2d66d3535cc920c566234d9b44
jugglingdev <kaylamarie1785@gmail.com>
2024-02-29 18:37:43 -0600

    Add API URL to environments

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,6 @@ export const environment = {
   firebaseUrl: 'https://whirler-6dbae-default-rtdb.firebaseio.com/',
   production: false,
   signupUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAGHDpvUo1PpymtFENUw9iOaosowOApa9Q',
-  loginUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAGHDpvUo1PpymtFENUw9iOaosowOApa9Q'
+  loginUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAGHDpvUo1PpymtFENUw9iOaosowOApa9Q',
+  apiUrl: 'http://localhost:3000'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,6 @@ export const environment = {
   firebaseUrl: 'https://whirler-6dbae-default-rtdb.firebaseio.com/',
   production: true,
   signupUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAGHDpvUo1PpymtFENUw9iOaosowOApa9Q',
-  loginUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAGHDpvUo1PpymtFENUw9iOaosowOApa9Q'
+  loginUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAGHDpvUo1PpymtFENUw9iOaosowOApa9Q',
+  apiUrl: 'http://whirler.com'
 };
