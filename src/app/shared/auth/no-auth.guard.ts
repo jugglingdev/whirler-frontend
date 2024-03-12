e49f22668d54959fc8ce900952c4b66ede6c6e16
jugglingdev <kaylamarie1785@gmail.com>
2024-03-11 20:25:47 -0500

    Create NoAuthGuard

---
+++ src/app/shared/auth/no-auth.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NoAuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean | UrlTree {
+    if (this.authService.isLoggedIn()) {
+      return this.router.createUrlTree(['/dashboard']);
+    } else {
+      return true;
+    }
+  }
+}
