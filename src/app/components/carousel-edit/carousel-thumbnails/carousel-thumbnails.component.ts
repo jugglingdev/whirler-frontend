e96cfb30b34e643895b1f9086f291d9f279ea9e9
jugglingdev <kaylamarie1785@gmail.com>
2024-04-09 21:26:38 -0500

    Update Carousel Thumbnail to store carouselId and slides array

--- src/app/components/carousel-edit/carousel-thumbnails/carousel-thumbnails.component.ts
+++ src/app/components/carousel-edit/carousel-thumbnails/carousel-thumbnails.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Slide } from 'src/app/models/slide';
+import { SlideService } from 'src/app/services/slide.service';
 
 @Component({
   selector: 'app-carousel-thumbnails',
   templateUrl: './carousel-thumbnails.component.html',
   styleUrls: ['./carousel-thumbnails.component.scss']
 })
-export class CarouselThumbnailsComponent {
+export class CarouselThumbnailsComponent implements OnInit {
+  @Input() carouselId: number;
+  slides: Slide[];
 
+  constructor(private slideService: SlideService) {}
+
+  ngOnInit(): void {
+    this.slideService.getSlides(this.carouselId).subscribe((slides) => {
+      this.slides = slides;
+    })
+  }
 }
