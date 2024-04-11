b2451c69e6ecae3658405dddcd6afce905ae0b7d
jugglingdev <kaylamarie1785@gmail.com>
2024-04-10 23:12:20 -0500

    Update environments apiUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,4 @@
 export const environment = {
-  firebaseUrl: 'https://whirler-6dbae-default-rtdb.firebaseio.com/',
   production: true,
-  signupUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAGHDpvUo1PpymtFENUw9iOaosowOApa9Q',
-  loginUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAGHDpvUo1PpymtFENUw9iOaosowOApa9Q',
-  apiUrl: 'http://whirler.com'
+  apiUrl: 'https://whirler.onrender.com/'
 };
