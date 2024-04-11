50854eed6515d1b502542ff59cbece1473bdd2bd
jugglingdev <kaylamarie1785@gmail.com>
2024-04-11 18:35:44 -0500

    Update Slide model

--- src/app/models/slide.ts
+++ src/app/models/slide.ts
@@ -1,16 +1,13 @@
 import { Carousel } from "./carousel";
-import { QuillContent } from "./quill-content";
 
 export class Slide {
   id?: number;
   title: string;
-  quillContents: QuillContent;
   carousel: Carousel;
 
   constructor (slide: any) {
     this.id = slide.id || 0;
     this.title = slide.title || '';
-    this.quillContents = slide.quillContents || new QuillContent({});
     this.carousel = slide.carousel || new Carousel({});
   }
 }
